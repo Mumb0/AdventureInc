a717040c806082b4e2d99722cd8b9b942406d4fe
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2023-07-08 11:32:15 +0200

    add pot scripts

--- Assets/GMTK2023/Game/Code/Minigames/Pots/PotMiniGame.cs
+++ Assets/GMTK2023/Game/Code/Minigames/Pots/PotMiniGame.cs
@@ -1,8 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
 
 namespace GMTK2023.Game.MiniGames {
 
 	public class PotMiniGame : MiniGame {
+
+#region Fields
+
+		[SerializeField] private RectTransform[]? potLocations;
+		[SerializeField] private GameObject? potPrefab;
+
+#endregion
+
+#region Properties
+
+		public IList<Pot>? ActivePots { get; set; }
+
+#endregion
 
 #region Methods
 
@@ -10,7 +26,15 @@ namespace GMTK2023.Game.MiniGames {
 			SetupRoom();
 		}
 
-		public void SetupRoom() { }
+		public void SetupRoom() {
+
+			ActivePots = new Collection<Pot>();
+
+			foreach (RectTransform rt in potLocations) {
+				ActivePots?.Add(Instantiate(potPrefab, rt.anchoredPosition, Quaternion.identity).GetComponent<Pot>());
+			}
+
+		}
 
 		public override void OnAdventurerEntered() {
 			throw new NotImplementedException();
