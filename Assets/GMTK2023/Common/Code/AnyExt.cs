0ad920e846f3559d6e9170e128bb090f694a851f
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 12:15:31 +0200

    feat: do util

---
+++ Assets/GMTK2023/Common/Code/AnyExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GMTK2023
+{
+    public static class AnyExt
+    {
+        public static void Do<T>(this T item, Action<T> action)
+        {
+            action(item);
+        }
+    }
+}
