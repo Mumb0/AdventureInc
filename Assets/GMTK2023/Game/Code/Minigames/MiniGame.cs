fdb2b395b3a2aa2f64d5138cbc97a7b057ce5e7f
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2023-07-08 09:57:14 +0200

    add minigame class

---
+++ Assets/GMTK2023/Game/Code/Minigames/MiniGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GMTK2023.Game.MiniGames {
+
+	public abstract class MiniGame : MonoBehaviour {
+
+		
+		
+		
+
+	}
+
+}
---
+++ Assets/GMTK2023/Game/Code/Minigames/Pots/PotMiniGame.cs
@@ -0,0 +1,13 @@
+using GMTK2023.Game.MiniGames;
+using UnityEngine;
+
+namespace GMTK2023.Game.MiniGames {
+
+	public class PotMiniGame : MiniGame {
+
+		
+		
+
+	}
+
+}
