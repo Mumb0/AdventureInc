3ae31a7b66e2a2c056e2adf246bbaa97923da1c2
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 13:09:11 +0200

    feat: locations

---
+++ Assets/GMTK2023/Game/Code/Map/LocationAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GMTK2023.Game
+{
+    [CreateAssetMenu(fileName = "new Location", menuName = "GMTK2023/Location")]
+    public class LocationAsset : ScriptableObject, ILocation
+    {
+        // NOTE: Use asset name
+        public string Name => name;
+    }
+}
---
+++ Assets/GMTK2023/Game/Code/Map/LocationDb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTK2023.Game
+{
+    public static class LocationDb
+    {
+        private const string Path = "Locations";
+
+
+        public static IReadOnlyCollection<ILocation> LoadLocations() =>
+            Resources.LoadAll<LocationAsset>(Path);
+    }
+}
