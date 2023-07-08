fb549c80f2d382360f5c7688ffbbb4e4e188f670
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 10:51:57 +0200

    feat: load shifts-info from assets

---
+++ Assets/GMTK2023/Game/Code/Shift/ShiftDb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GMTK2023.Game
+{
+    /// <summary>
+    /// IO functions for loading shift-infos
+    /// </summary>
+    public static class ShiftDb
+    {
+        private const string ShiftPath = "Shifts/";
+
+
+        /// <summary>
+        /// Attempts to load a shift-info
+        /// </summary>
+        /// <param name="index">The index. 0-based</param>
+        /// <returns>The info if found</returns>
+        public static IShiftInfo? TryLoadShiftByIndex(int index)
+        {
+            // NOTE: For this to work shift-assets need to be named "Shift 1" etc
+            var assetName = $"Shift {index + 1}";
+            var path = ShiftPath + assetName;
+            var asset = Resources.Load<ShiftInfoAsset>(path);
+            return asset ? asset : null;
+        }
+    }
+}
