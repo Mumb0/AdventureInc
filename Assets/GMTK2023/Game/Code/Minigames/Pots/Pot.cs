c9dcc910ae33569da478e9f6cc165bd17b07d07b
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2023-07-08 11:06:57 +0200

    add pot scripts

---
+++ Assets/GMTK2023/Game/Code/Minigames/Pots/Pot.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace GMTK2023.Game.MiniGames {
+
+	public class Pot : MonoBehaviour {
+
+#region Fields
+
+		[SerializeField] private GameObject potPiecePrefab;
+		[SerializeField] private float pieceRadius;
+
+#endregion
+
+#region Properties
+
+		public bool IsFilledWithCoins { get; set; }
+
+#endregion
+
+#region Methods
+
+		public void Smash() {
+
+			int brokenPieces = Random.Range(3, 6);
+
+			for (int i = 0; i < brokenPieces; i++) {
+				
+			}
+
+		}
+
+#endregion
+
+	}
+
+}
--- Assets/GMTK2023/Game/Code/Minigames/Pots/PotMiniGame.cs
+++ Assets/GMTK2023/Game/Code/Minigames/Pots/PotMiniGame.cs
@@ -1,8 +1,24 @@
+using System;
+
 namespace GMTK2023.Game.MiniGames {
 
 	public class PotMiniGame : MiniGame {
 
-#region Fields
+#region Methods
+
+		public void Awake() {
+			SetupRoom();
+		}
+
+		public void SetupRoom() { }
+
+		public override void OnAdventurerEntered() {
+			throw new NotImplementedException();
+		}
+
+		public override void OnAdventurerLeft() {
+			throw new NotImplementedException();
+		}
 
 #endregion
 
