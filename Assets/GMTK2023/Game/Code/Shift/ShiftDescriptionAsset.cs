5648e11300d497eea0f4472693b04ef902d14163
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 11:22:22 +0200

    feat: adventurer enter-time data

--- Assets/GMTK2023/Game/Code/Shift/ShiftDescriptionAsset.cs
+++ Assets/GMTK2023/Game/Code/Shift/ShiftDescriptionAsset.cs
@@ -7,10 +7,10 @@ namespace GMTK2023.Game
     [CreateAssetMenu(fileName = "New Shift-description", menuName = "GMTK2023/Shift")]
     public class ShiftInfoAsset : ScriptableObject, IShiftInfo
     {
-        [SerializeField] private AdventurerInfoAsset[] adventurers =
-            Array.Empty<AdventurerInfoAsset>();
+        [SerializeField] private AdventurerInShift[] adventurers =
+            Array.Empty<AdventurerInShift>();
 
 
-        public IReadOnlyList<IAdventurerInfo> Adventurers => adventurers;
+        public IReadOnlyList<AdventurerInShift> Adventurers => adventurers;
     }
 }
--- Assets/GMTK2023/Game/Code/Shift/Types.cs
+++ Assets/GMTK2023/Game/Code/Shift/Types.cs
@@ -1,7 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace GMTK2023.Game
 {
+    /// <summary>
+    /// Describes an adventurer in a shift
+    /// </summary>
+    [Serializable]
+    public class AdventurerInShift
+    {
+        [SerializeField] private AdventurerInfoAsset? adventurerInfo;
+        [SerializeField] private float enterTimeSeconds;
+
+
+        /// <summary>
+        /// The adventurer
+        /// </summary>
+        public IAdventurerInfo AdventurerInfo => adventurerInfo!;
+
+        /// <summary>
+        /// After what time does this adventurer enter the shift
+        /// </summary>
+        public TimeSpan EnterTime => TimeSpan.FromSeconds(enterTimeSeconds);
+    }
+
+
     /// <summary>
     /// Describes a shift. Equivalent to a level or night in FNAF
     /// </summary>
@@ -10,6 +34,6 @@ namespace GMTK2023.Game
         /// <summary>
         /// The adventurers that will be active during this shift
         /// </summary>
-        public IReadOnlyList<IAdventurerInfo> Adventurers { get; }
+        public IReadOnlyList<AdventurerInShift> Adventurers { get; }
     }
 }
