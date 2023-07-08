0a10700e87f7bc7d13bc2dd574479428482d2f7d
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 10:01:31 +0200

    refactor: adventurer-info asset
    use asset-name as adventurer title

--- Assets/GMTK2023/Game/Code/Adventurers/AdventurerInfoAsset.cs
+++ Assets/GMTK2023/Game/Code/Adventurers/AdventurerInfoAsset.cs
@@ -5,9 +5,7 @@ namespace GMTK2023.Game
     [CreateAssetMenu(fileName = "New Adventurer-info", menuName = "GMTK2023/Adventurer-info")]
     public class AdventurerInfoAsset : ScriptableObject, IAdventurerInfo
     {
-        [SerializeField] private string title = "";
-
-
-        public string Title => title;
+        /// <remarks>We use the asset name as the adventurers title</remarks>>
+        public string Title => name;
     }
 }
