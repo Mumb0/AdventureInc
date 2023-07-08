cb437b8777c035f634d546053adeec1356e37c9c
Ramon Brullo <kingvanti650@gmail.com>
2023-07-08 12:19:15 +0200

    feat: iter util

---
+++ Assets/GMTK2023/Common/Code/IEnumerableExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GMTK2023
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableExt
+    {
+        public static void Iter<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
