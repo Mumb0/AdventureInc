8e6edadd0a460b1c6271d74200cb38b6f5b54dea
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2023-07-09 10:58:18 +0200

    add adventurer display

--- Assets/GMTK2023/Game/Code/Map/LocationDisplay.cs
+++ Assets/GMTK2023/Game/Code/Map/LocationDisplay.cs
@@ -8,6 +8,28 @@ namespace GMTK2023.Game {
 
 		[SerializeField] private Image[] adventurerSlots = Array.Empty<Image>();
 
+		private int currentAdventurers = 0;
+
+		public int CurrentAdventurers {
+			get => currentAdventurers;
+			set {
+				currentAdventurers = value;
+				UpdateAdventurerSlots();
+			}
+		}
+
+		private void UpdateAdventurerSlots() {
+
+			foreach (Image t in adventurerSlots) {
+				t.enabled = false;
+			}
+
+			for (int i = 0; i < CurrentAdventurers; i++) {
+				adventurerSlots[i].enabled = true;
+			}
+
+		}
+
 	}
 
 }
--- Assets/GMTK2023/Game/Code/Map/MapDisplay.cs
+++ Assets/GMTK2023/Game/Code/Map/MapDisplay.cs
@@ -1,21 +1,72 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 namespace GMTK2023.Game {
 
 	public class MapDisplay : MonoBehaviour {
 
+#region Constants
+
+		public static int MaxAdventurers = 4;
+
+#endregion
+
+#region Fields
+
 		[SerializeField] private LocationDisplayLink[] locationDisplays = Array.Empty<LocationDisplayLink>();
+		[SerializeField] private Color32[] adventurerColors = Array.Empty<Color32>();
+
+		private IList<Adventurer> ActiveAdventurers { get; set; } = new Collection<Adventurer>();
+		private Dictionary<Adventurer, ILocation> LocationLog = new Dictionary<Adventurer, ILocation>();
+
+#endregion
+
+#region Properties
 
 		public Dictionary<ILocation, LocationDisplay>? Locations { get; } = new Dictionary<ILocation, LocationDisplay>();
 
-		private void Start() {
+#endregion
+
+		private void Awake() {
 
 			foreach (LocationDisplayLink ldl in locationDisplays) {
 				Locations?.Add(ldl.Location, ldl.LocationDisplay);
 			}
 
+			Singleton.TryFind<AdventurerManager>()!.AdventurerEntered += OnAdventurerEnteredWorld;
+			Singleton.TryFind<TravelManager>()!.AdventurerLocationStart += OnAdventurerStarted;
+			Singleton.TryFind<TravelManager>()!.AdventurerChangedLocation += OnAdventurerMoved;
+		}
+
+		private void OnAdventurerEnteredWorld(IAdventurerTracker.AdventurerEnteredEvent e) {
+			ActiveAdventurers.Add(e.Adventurer);
+		}
+
+		private void OnAdventurerStarted(IAdventurerLocationTracker.AdventurerLocationStartEvent e) {
+			if (ActiveAdventurers.Contains(e.Adventurer)) {
+
+				if (Locations?[e.Location].CurrentAdventurers < MaxAdventurers) {
+					Locations[e.Location].CurrentAdventurers++;
+					LocationLog.Add(e.Adventurer, e.Location);
+				}
+
+			}
+		}
+
+		private void OnAdventurerMoved(IAdventurerLocationTracker.AdventurerChangedLocationEvent e) {
+
+			if (ActiveAdventurers.Contains(e.Adventurer)) {
+				if (Locations?[e.Location].CurrentAdventurers < MaxAdventurers) {
+
+					Locations[LocationLog[e.Adventurer]].CurrentAdventurers--;
+
+					Locations[e.Location].CurrentAdventurers++;
+
+					LocationLog[e.Adventurer] = e.Location;
+				}
+			}
 		}
 
 	}
