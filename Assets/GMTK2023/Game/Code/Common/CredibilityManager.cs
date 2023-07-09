e3db60f96c8029a7b50acf16393dfe5cdd160117
Ramon Brullo <kingvanti650@gmail.com>
2023-07-09 11:23:10 +0200

    feat: track credibility

---
+++ Assets/GMTK2023/Game/Code/Common/CredibilityManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace GMTK2023.Game.GMTK2023.Game.Code.Common
+{
+    public class CredibilityManager : MonoBehaviour, ICredibilityTracker
+    {
+        public event Action<ICredibilityTracker.CredibilityChangedEvent>? CredibilityChanged;
+
+
+        private float credibility;
+
+
+        private float Credibility
+        {
+            get => credibility;
+            set
+            {
+                credibility = value;
+                CredibilityChanged?.Invoke(new ICredibilityTracker.CredibilityChangedEvent(value));
+            }
+        }
+
+
+        private void OnShiftStarted(IShiftProgressTracker.ShiftStartedEvent _)
+        {
+            Credibility = 1;
+        }
+
+        private void Awake()
+        {
+            Singleton.TryFind<IShiftProgressTracker>()!.ShiftStarted += OnShiftStarted;
+        }
+    }
+}
