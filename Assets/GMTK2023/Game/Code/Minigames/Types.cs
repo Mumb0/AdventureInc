8ae35ca29dc1614d3043244d680a29094b17d48f
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2023-07-09 13:45:51 +0200

    add

--- Assets/GMTK2023/Game/Code/Minigames/Types.cs
+++ Assets/GMTK2023/Game/Code/Minigames/Types.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
-namespace GMTK2023.Game.MiniGames
-{
-    public interface IMiniGame
-    {
-        public string Name { get; }
+namespace GMTK2023.Game.MiniGames {
 
-        public bool IsCredible { get; }
+	public interface IMiniGame {
 
-        public TimeSpan Duration { get; }
-    }
+		public string Name { get; }
 
-    public interface IMiniGameTracker
-    {
-        public IReadOnlyList<IMiniGame> AllMiniGames { get; }
-    }
+		public bool IsCredible { get; }
+
+		public TimeSpan Duration { get; }
+		public bool IsPrepared { get; set; }
+		public int CurrentTaskStep { get; set; }
+		public MiniGameTask[] MiniGameTasks { get; }
+		public Camera? MainCamera { get; }
+
+		public void SetActive();
+
+		public void OnAdventurerEntered();
+
+		public void OnAdventurerLeft();
+		
+		public event Action? AllMiniGameTasksCompleted;
+
+		public event Action<int>? MiniGameTaskCompleted;
+
+		public event Action? AdventurerEnteredUnpreparedRoom;
+
+	}
+
+	public interface IMiniGameTracker {
+
+		public IReadOnlyList<IMiniGame> AllMiniGames { get; }
+
+	}
+
 }
