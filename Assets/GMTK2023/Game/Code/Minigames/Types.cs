15f6fd20cfa088e20376b805f6f0abbaa238ab1a
Ramon Brullo <kingvanti650@gmail.com>
2023-07-09 09:07:06 +0200

    feat: add mini-game duration !breaking

--- Assets/GMTK2023/Game/Code/Minigames/Types.cs
+++ Assets/GMTK2023/Game/Code/Minigames/Types.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GMTK2023.Game.MiniGames
 {
     public interface IMiniGame
     {
         public string Name { get; }
+
+        public TimeSpan Duration { get; }
     }
 
     public interface IMiniGameTracker
