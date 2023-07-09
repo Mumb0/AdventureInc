14b6aef29967c8c8f78e5d18be9fbe554cfd56ee
Ramon Brullo <kingvanti650@gmail.com>
2023-07-09 17:07:21 +0200

    feat: add success scene

--- Assets/GMTK2023/Game/Code/Common/GameManager.cs
+++ Assets/GMTK2023/Game/Code/Common/GameManager.cs
@@ -2,47 +2,59 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace GMTK2023.Game {
+namespace GMTK2023.Game
+{
+    /// <summary>
+    /// Top-level game-manager.
+    /// Responsible for starting/stopping the game
+    /// </summary>
+    public class GameManager : MonoBehaviour, IGameLoader, IGameOverTracker
+    {
+        public event Action<IGameLoader.GameLoadEvent>? GameLoaded;
 
-	/// <summary>
-	/// Top-level game-manager.
-	/// Responsible for starting/stopping the game
-	/// </summary>
-	public class GameManager : MonoBehaviour, IGameLoader, IGameOverTracker {
+        public event Action<IGameOverTracker.GameOverEvent>? GameOver;
 
-		public event Action<IGameLoader.GameLoadEvent>? GameLoaded;
 
-		public event Action<IGameOverTracker.GameOverEvent>? GameOver;
+        private async void Start()
+        {
+            // Load game or start new if there is none
+            var savedGame = await GameSaving.TryLoadSavedGameAsync()
+                            ?? await GameSaving.StartNewGameAsync();
 
-		private async void Start() {
-			// Load game or start new if there is none
-			var savedGame = await GameSaving.TryLoadSavedGameAsync()
-			                ?? await GameSaving.StartNewGameAsync();
+            GameLoaded?.Invoke(new IGameLoader.GameLoadEvent(savedGame));
+        }
 
-			GameLoaded?.Invoke(new IGameLoader.GameLoadEvent(savedGame));
-		}
+        private void HandleGameOver()
+        {
+            GameOver?.Invoke(new IGameOverTracker.GameOverEvent());
 
-		private void HandleGameOver() {
-			GameOver?.Invoke(new IGameOverTracker.GameOverEvent());
+            // TODO: Go to game over screen instead
+            SceneManager.LoadScene(0); // Menu
+        }
 
-			// TODO: Go to game over screen instead
-			SceneManager.LoadScene(0); // Menu
-		}
+        private void OnCredibilityChanged(ICredibilityTracker.CredibilityChangedEvent e)
+        {
+            if (e.Credibility > 0) return;
+            HandleGameOver();
+        }
 
-		private void OnCredibilityChanged(ICredibilityTracker.CredibilityChangedEvent e) {
-			if (e.Credibility > 0) return;
-			HandleGameOver();
-		}
+        private void OnShiftCompleted(IShiftProgressTracker.ShiftCompletedEvent obj)
+        {
+            SceneManager.LoadScene(2); // Success
+        }
 
-		private void Awake() {
-			Singleton.TryFind<ICredibilityTracker>()!.CredibilityChanged +=
-				OnCredibilityChanged;
-		}
+        private void Awake()
+        {
+            Singleton.TryFind<ICredibilityTracker>()!.CredibilityChanged +=
+                OnCredibilityChanged;
+            Singleton.TryFind<IShiftProgressTracker>()!.ShiftCompleted +=
+                OnShiftCompleted;
+        }
 
-		public void Quit() {
-			Application.Quit();
-		}
 
-	}
-
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
 }
