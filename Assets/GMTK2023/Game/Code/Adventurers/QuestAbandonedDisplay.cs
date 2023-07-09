81fac77788cad6c8715bc91d7fc64cfaf31d1b29
Ramon Brullo <kingvanti650@gmail.com>
2023-07-09 15:50:13 +0200

    feat: hide abandon message after time

--- Assets/GMTK2023/Game/Code/Adventurers/QuestAbandonedDisplay.cs
+++ Assets/GMTK2023/Game/Code/Adventurers/QuestAbandonedDisplay.cs
@@ -1,17 +1,31 @@
+using System;
 using TMPro;
 using UnityEngine;
+using static GMTK2023.Game.TimeUtil;
 
 namespace GMTK2023.Game
 {
     public class QuestAbandonedDisplay : MonoBehaviour
     {
-        private TMP_Text label = null!;
+        [SerializeField] private float hideTimeInSeconds;
 
+        private TMP_Text label = null!;
+        private TimeSpan lastMessageTime;
+
+        private TimeSpan HideTime => TimeSpan.FromSeconds(hideTimeInSeconds);
+
+
+        private void Update()
+        {
+            var elapsed = TimeSinceUnityStart - lastMessageTime;
+            label.enabled = elapsed < HideTime;
+        }
 
         private void OnQuestAbandoned(IQuestTracker.QuestAbandonedEvent e)
         {
             var miniGame = e.Quest.MiniGame;
             var text = $"Attention!\n{e.Adventurer.Info.Title} could not {miniGame.ActivityDescription} because {miniGame.AbandonmentReason}";
+            lastMessageTime = TimeSinceUnityStart;
             label.text = text;
         }
 
